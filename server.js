4add049281a04f12cf29b08ae70bb4374e7f2895
Ruturaj <ruturaj.ghodke77@gmail.com>
2020-06-08 12:00:26 +0530

    added config file for env  variables

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const dotenv =  require('dotenv').config({path: './config/config.env'})
+
 
 const app = express()
 
-mongoose.connect('mongodb://localhost:27017/mernTest', {useNewUrlParser: true, useUnifiedTopology: true},(err) =>{
+mongoose.connect(process.env.MONGO_URI_OFFLINE, {useNewUrlParser: true, useUnifiedTopology: true},(err) =>{
     if(err) {
         console.log(`Error while connecting to MongoDB: ${err}`)
     }
